8c2c3835b44b588580feb9b05711baa06b8b1188
gouthami24 <gouthami.santhanam@gmail.com>
2024-05-20 14:46:22 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,6 @@ size = st.selectbox('Select size:', ['2','3','4'])
 level = st.selectbox('Select level:', ['Easy','Medium','Hard'])  
 
 #Calling the Sudoku
-sudoku_puzzle = generate_sudoku(size, level)
-st.write('The Sudoku Puzzle is', sudoku_puzzle)
+generate_sudoku(size, level)
+#st.write('The Sudoku Puzzle is', sudoku_puzzle)
     
