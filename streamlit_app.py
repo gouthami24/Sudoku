aee76c9f454b9d2cfbc2e3efbf804b8542f24e45
gouthami24 <gouthami.santhanam@gmail.com>
2024-05-20 15:30:30 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,138 @@ def generate_sudoku(size, level):
     return (response)
     
 #sudoku = generate_sudoku(size,level)
+# Define the HTML for the Sudoku puzzle
+sudoku_html = """
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 300px;
+            height: 300px;
+        }
+        td {
+            border: 1px solid black;
+            width: 33px;
+            height: 33px;
+            text-align: center;
+            font-size: 24px;
+        }
+        .bold-border {
+            border-width: 2px;
+        }
+    </style>
+</head>
+<body>
+    <h2>3x3 Sudoku Puzzle</h2>
+    <table>
+        <tr>
+            <td class="bold-border"></td>
+            <td class="bold-border">2</td>
+            <td class="bold-border"></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td class="bold-border">6</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td class="bold-border">3</td>
+        </tr>
+        <tr>
+            <td class="bold-border"></td>
+            <td class="bold-border">7</td>
+            <td class="bold-border">4</td>
+            <td></td>
+            <td class="bold-border">8</td>
+            <td></td>
+            <td class="bold-border">2</td>
+            <td class="bold-border"></td>
+            <td class="bold-border"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td class="bold-border">6</td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td class="bold-border">2</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td class="bold-border">1</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+</body>
+</html>
+"""
 
+# Embed the HTML into the Streamlit app
+st.components.v1.html(sudoku_html, height=500)
 
 # User input
 size = st.selectbox('Select size:', ['2','3','4'])  
@@ -30,7 +161,8 @@ st.write(level)
 #generate_sudoku(size, level)
 #st.write('The Sudoku Puzzle is', sudoku_puzzle)
 if st.button('Generate'):
-    sudoku=generate_sudoku(size,level)
+    #sudoku=generate_sudoku(size,level)
+    st.components.v1.html(sudoku_html, height=600)
 else:
     st.write("Sorry Cannot Generate")
 st.write("Generated Sudoku puzzle:")
